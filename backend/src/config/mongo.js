5972944ab863b8d61426532b2a87864d96f0b160
Auder20 <audergonzalez@gmail.com>
2025-07-09 16:58:29 -0500

    version 1.9.2

--- backend/src/config/mongo.js
+++ backend/src/config/mongo.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectMongo = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost:27017/Edupro');
+   await mongoose.connect(process.env.MONGODB_URI);
     console.log('MongoDB conectado');
   } catch (error) {
     console.error('Error conectando a MongoDB:', error);
