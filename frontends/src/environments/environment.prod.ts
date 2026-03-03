04b724ea892a7473a9db3022259f61c3923fc00c
Auder20 <audergonzalez@gmail.com>
2026-03-03 13:53:55 -0500

    2.1.1

--- frontends/src/environments/environment.prod.ts
+++ frontends/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://TU_BACKEND_PRODUCCION/api' // Cambia esta URL por la de tu backend en producción
+  apiUrl: 'https://edupro-xzdj.onrender.com/api'
 };
